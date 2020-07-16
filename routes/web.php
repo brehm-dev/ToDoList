0474dcfae67309062fd5e3c9a2b02ae983081ad1
Andreas Brehm <andreasbrehm@live.de>
2020-07-16 20:48:47 +0200

    remove RegisterController.php and register route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@
 Route::get('/', function () {
     return view('dashboard');
 });
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
