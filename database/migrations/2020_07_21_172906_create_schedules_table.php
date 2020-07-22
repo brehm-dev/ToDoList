48ae60b650ffb32037a1b489f07e38a1c0a7d01f
Andreas Brehm <andreasbrehm@live.de>
2020-07-23 00:10:05 +0200

    schedules migration fix

--- database/migrations/2020_07_21_172906_create_schedules_table.php
+++ database/migrations/2020_07_21_172906_create_schedules_table.php
@@ -16,7 +16,7 @@ class CreateSchedulesTable extends Migration
         Schema::create('schedules', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('visibility')->nullable()->default('ROLE_USER');
+            $table->string('visibility')->default('ROLE_USER');
             $table->string('description');
             $table->timestamps();
         });
